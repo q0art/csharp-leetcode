65ced36ced2decb472781184ad23b38b2d849801
Artem Merkulov <qtqt.art@gmail.com>
2023-12-08 00:17:40 +0300

    9. Palindrome Number sol

---
+++ csharp-leetcode/easy/9.Palindrome_Number/Solution.cs
@@ -0,0 +1,18 @@
+namespace csharp_leetcode.easy._9.Palindrome_Number;
+
+public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        var s = x.ToString();
+
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != s[s.Length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- csharp-leetcode/obj/Debug/net8.0/csharp-leetcode.AssemblyInfo.cs
+++ csharp-leetcode/obj/Debug/net8.0/csharp-leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a0c5be829ed704451c518950d3b517aad2b904")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
