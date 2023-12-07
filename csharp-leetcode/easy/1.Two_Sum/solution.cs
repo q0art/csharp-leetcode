47a0c5be829ed704451c518950d3b517aad2b904
Artem Merkulov <qtqt.art@gmail.com>
2023-12-07 22:34:19 +0300

    add 1. Two Sum sol

---
+++ csharp-leetcode/easy/1.Two_Sum/solution.cs
@@ -0,0 +1,22 @@
+namespace csharp_leetcode.easy._1.Two_Sum;
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+
+            if (dict.ContainsKey(diff))
+            {
+                return new int[] { dict[diff], i };
+            }
+            
+            dict[nums[i]] = i;
+        }
+
+        return new int[0];
+    }
+}
