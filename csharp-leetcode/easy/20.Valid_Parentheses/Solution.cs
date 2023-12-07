1d6b4a03d22873a72f3c7f01565de65bdf07e79d
Artem Merkulov <qtqt.art@gmail.com>
2023-12-08 01:25:20 +0300

    20. Valid Parentheses sol

---
+++ csharp-leetcode/easy/20.Valid_Parentheses/Solution.cs
@@ -0,0 +1,32 @@
+namespace csharp_leetcode.easy._20.Valid_Parentheses;
+
+public class Solution {
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        var dict = new Dictionary<char, char>()
+        {
+            { '{', '}' },
+            { '[', ']' },
+            { '(', ')' }
+        };
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.Keys.Contains(s[i]))
+            {
+                stack.Push(s[i]);
+            }
+            else if (stack.Count > 0 && s[i] == dict[stack.Peek()])
+            {
+                stack.Pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
--- csharp-leetcode/obj/Debug/net8.0/csharp-leetcode.AssemblyInfo.cs
+++ csharp-leetcode/obj/Debug/net8.0/csharp-leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39566566d9ff37c16e83ec8fb6a81779373f41c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3537d05223f217af9d70d8c4e865672afd104b59")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
