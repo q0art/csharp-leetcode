3537d05223f217af9d70d8c4e865672afd104b59
Artem Merkulov <qtqt.art@gmail.com>
2023-12-08 00:46:15 +0300

    add 14. Longest Common Prefix sol

---
+++ csharp-leetcode/easy/14.Longest_Common_Prefix/Solution.cs
@@ -0,0 +1,24 @@
+namespace csharp_leetcode.easy._14.Longest_Common_Prefix;
+
+public class Solution {
+    public string LongestCommonPrefix(string[] s)
+    {
+        Array.Sort(s);
+
+        string prefix = "";
+
+        for (int i = 0; i < s[0].Length; i++)
+        {
+            if (s[0][i] != s[s.Length - 1][i])
+            {
+                return prefix;
+            }
+            else
+            {
+                prefix += s[0][i];
+            }
+        }
+
+        return prefix;
+    }
+}
