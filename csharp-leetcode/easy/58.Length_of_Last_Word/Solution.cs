fd8cde44aa4c07c76f7caf6d6350838075eb0ed9
Artem Merkulov <qtqt.art@gmail.com>
2023-12-09 00:29:00 +0300

    add 58. Length of Last Word sol

---
+++ csharp-leetcode/easy/58.Length_of_Last_Word/Solution.cs
@@ -0,0 +1,8 @@
+namespace csharp_leetcode.easy._58.Length_of_Last_Word;
+
+public class Solution {
+    public int LengthOfLastWord(string s)
+    {
+        return s.Trim().Split(' ').Last().Length;
+    }
+}
