86c5ff24f3648e95f22c095c5e831b9121437218
Artem Merkulov <qtqt.art@gmail.com>
2023-12-09 20:49:01 +0300

    add 69. Sqrt(x) sol

---
+++ csharp-leetcode/easy/69.Sqrt(x)/Solution.cs
@@ -0,0 +1,21 @@
+namespace csharp_leetcode.easy._69.Sqrt_x_;
+
+public class Solution
+{
+    public int MySqrt(int x)
+    {
+        if (x == 1)
+        {
+            return 1;
+        }
+
+        double res = x / 2;
+
+        while (res * res - x > 0.5)
+        {
+            res = (res + x / res) / 2;
+        }
+
+        return (int)res;
+    }
+}
