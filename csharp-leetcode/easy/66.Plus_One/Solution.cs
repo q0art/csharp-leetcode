fb16c1aed76607cf13507e034af69da6db11a2db
Artem Merkulov <qtqt.art@gmail.com>
2023-12-09 20:11:21 +0300

    add 66. Plus One sol

---
+++ csharp-leetcode/easy/66.Plus_One/Solution.cs
@@ -0,0 +1,22 @@
+namespace csharp_leetcode.easy._66.Plus_One;
+
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        var result = new int[digits.Length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+}
