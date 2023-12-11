16812c814e5d9d963d117c731c4965409b59e78b
Artem Merkulov <qtqt.art@gmail.com>
2023-12-11 16:04:06 +0300

    add 104. Maximum Depth of Binary Tree sol

---
+++ csharp-leetcode/easy/104.Maximum_Depth_of_Binary_Tree/Solution.cs
@@ -0,0 +1,30 @@
+namespace csharp_leetcode.easy._104.Maximum_Depth_of_Binary_Tree;
+
+public class TreeNode 
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) 
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public int MaxDepth(TreeNode root) 
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int left = MaxDepth(root.left), right = MaxDepth(root.right);
+
+        return Math.Max(left, right) + 1;
+    }
+}
