ee711fe55046410815afe28a564a30124c90d18f
Artem Merkulov <qtqt.art@gmail.com>
2023-12-12 01:06:21 +0300

    add 112. Path Sum sol

---
+++ csharp-leetcode/easy/112.Path_Sum/Solution.cs
@@ -0,0 +1,21 @@
+using csharp_leetcode.helpers;
+
+namespace csharp_leetcode.easy._112.Path_Sum;
+
+public class Solution
+{
+    public bool HasPathSum(TreeNode root, int targetSum) 
+    {
+        if (root == null)
+        {
+            return false;
+        }
+
+        if (root.left == null && root.right == null && root.val - targetSum == 0)
+        {
+            return true;
+        }
+        
+        return HasPathSum(root.left,  targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
