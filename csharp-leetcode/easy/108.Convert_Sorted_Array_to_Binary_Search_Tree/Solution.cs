10556e1bd34adf265298778cfa3c3a192bd8c558
Artem Merkulov <qtqt.art@gmail.com>
2023-12-11 16:14:00 +0300

    add 108. Convert Sorted Array to Binary Search Tree sol

---
+++ csharp-leetcode/easy/108.Convert_Sorted_Array_to_Binary_Search_Tree/Solution.cs
@@ -0,0 +1,44 @@
+namespace csharp_leetcode.easy._108.Convert_Sorted_Array_to_Binary_Search_Tree;
+
+public class TreeNode 
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) 
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public TreeNode SortedArrayToBST(int[] nums) 
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            return null;
+        }
+
+        return SortedArrayToBSTHelper(nums, 0, nums.Length - 1);
+    }
+
+    private TreeNode SortedArrayToBSTHelper(int[] nums, int left, int right)
+    {
+        if (left > right)
+        {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+        var node = new TreeNode(nums[mid]);
+
+        node.left = SortedArrayToBSTHelper(nums, left, mid - 1);
+        node.right = SortedArrayToBSTHelper(nums, mid + 1, right);
+
+        return node;
+    }
+}
