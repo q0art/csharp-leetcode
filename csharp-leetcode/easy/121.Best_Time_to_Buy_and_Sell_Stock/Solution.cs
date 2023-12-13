4158554477ef445ffbfb3b1db950271f6857fc10
Artem Merkulov <qtqt.art@gmail.com>
2023-12-13 23:06:57 +0300

    add 121. Best Time to Buy and Sell Stock sol

---
+++ csharp-leetcode/easy/121.Best_Time_to_Buy_and_Sell_Stock/Solution.cs
@@ -0,0 +1,22 @@
+namespace csharp_leetcode.easy._121.Best_Time_to_Buy_and_Sell_Stock;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices) 
+    {
+        if (prices == null || prices.Length <= 1)
+        {
+            return 0;
+        }
+
+        int maxProfit = 0, minPrice = prices[0];
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            minPrice = Math.Min(minPrice, prices[i]);
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
