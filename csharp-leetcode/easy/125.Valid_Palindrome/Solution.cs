4f1dca487d9be13d16006070466c18113c46fc3c
Artem Merkulov <qtqt.art@gmail.com>
2023-12-13 23:18:04 +0300

    add 125. Valid Palindrome sol

---
+++ csharp-leetcode/easy/125.Valid_Palindrome/Solution.cs
@@ -0,0 +1,36 @@
+namespace csharp_leetcode.easy._125.Valid_Palindrome;
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        if (s == null || s.Length <= 1)
+        {
+            return true;
+        }
+
+        s = s.ToLower();
+        
+        var list = new List<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+            {
+                list.Add(c);
+            }
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] != list[list.Count - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
