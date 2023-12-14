3ea0d541fa1e3af9c329bb865e0cbba4ce59cf9e
Artem Merkulov <qtqt.art@gmail.com>
2023-12-14 17:25:58 +0300

    add 145. Binary Tree Postorder Traversal sol

---
+++ csharp-leetcode/easy/145.Binary_Tree_Postorder_Traversal/Solution.cs
@@ -0,0 +1,28 @@
+using csharp_leetcode.helpers;
+
+namespace csharp_leetcode.easy._145.Binary_Tree_Postorder_Traversal;
+
+public class Solution
+{
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+        var list = new List<int>();
+        
+        PostorderTraversalHelper(root, list);
+        
+        return list;
+    }
+
+    private void PostorderTraversalHelper(TreeNode node, List<int> list)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        
+        PostorderTraversalHelper(node.left, list);
+        PostorderTraversalHelper(node.right, list);
+        
+        list.Add(node.val);
+    }
+}
