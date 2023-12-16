3861d19643e9eadc214166466ec27fa2ed3f04a5
Artem Merkulov <qtqt.art@gmail.com>
2023-12-17 01:23:00 +0300

    add 217. Contains Duplicate sol

---
+++ csharp-leetcode/easy/217.Contains_Duplicate/Solution.cs
@@ -0,0 +1,21 @@
+namespace csharp_leetcode.easy._217.Contains_Duplicate;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (hashSet.Contains(nums[i]))
+            {
+                return true;
+            }
+
+            hashSet.Add(nums[i]);
+        }
+
+        return false;
+    }
+}
