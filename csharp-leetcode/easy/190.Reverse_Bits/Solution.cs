650774ad9563d5eb1981a108be038999e2d20a89
Artem Merkulov <qtqt.art@gmail.com>
2023-12-16 15:55:13 +0300

    add 190. Reverse Bits sol

---
+++ csharp-leetcode/easy/190.Reverse_Bits/Solution.cs
@@ -0,0 +1,28 @@
+namespace csharp_leetcode.easy._190.Reverse_Bits;
+
+public class Solution
+{
+    public uint reverseBits(uint n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        uint result = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+            result <<= 1;
+
+            if ((n & 1) == 1)
+            {
+                result++;
+            }
+
+            n >>= 1;
+        }
+
+        return result;
+    }
+}
