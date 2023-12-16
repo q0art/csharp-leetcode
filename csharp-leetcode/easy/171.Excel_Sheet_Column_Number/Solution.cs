376ed898732804875eedf492e8af797da1e6f7c9
Artem Merkulov <qtqt.art@gmail.com>
2023-12-16 15:45:42 +0300

    add 171. Excel Sheet Column Number sol

---
+++ csharp-leetcode/easy/171.Excel_Sheet_Column_Number/Solution.cs
@@ -0,0 +1,16 @@
+namespace csharp_leetcode.easy._171.Excel_Sheet_Column_Number;
+
+public class Solution
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        int result = 0;
+
+        for (int i = 0; i < columnTitle.Length; i++)
+        {
+            result = result * 26 + columnTitle[i] - 'A' + 1;
+        }
+
+        return result;
+    }
+}
