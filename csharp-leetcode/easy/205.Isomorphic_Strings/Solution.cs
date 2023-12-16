eb4287842f87398c7873b0fdb5b2644c87a71dca
Artem Merkulov <qtqt.art@gmail.com>
2023-12-16 16:21:30 +0300

    add 205. Isomorphic Strings sol

---
+++ csharp-leetcode/easy/205.Isomorphic_Strings/Solution.cs
@@ -0,0 +1,29 @@
+namespace csharp_leetcode.easy._205.Isomorphic_Strings;
+
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        var dict = new Dictionary<char, char>();
+        var hashSet = new HashSet<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.ContainsKey(s[i]))
+            {
+                if (dict[s[i]] != t[i]) return false;
+            }
+            else
+            {
+                if (hashSet.Contains(t[i])) return false;
+
+                dict[s[i]] = t[i];
+                hashSet.Add(t[i]);
+            }
+        }
+
+        return true;
+    }
+}
