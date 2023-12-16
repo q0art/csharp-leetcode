7100b4fae9d9c07509fb2207c96ce1e707711fe0
Artem Merkulov <qtqt.art@gmail.com>
2023-12-17 01:52:52 +0300

    add 226. Invert Binary Tree sol

---
+++ csharp-leetcode/easy/226.Invert_Binary_Tree/Solution.cs
@@ -0,0 +1,22 @@
+using csharp_leetcode.helpers;
+
+namespace csharp_leetcode.easy._226.Invert_Binary_Tree;
+
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root) {
+        if (root == null)
+        {
+            return null;
+        }
+
+        var current = root.left;
+        root.left = root.right;
+        root.right = current;
+
+        root.left = InvertTree(root.left);
+        root.right = InvertTree(root.right);
+
+        return root;
+    }
+}
