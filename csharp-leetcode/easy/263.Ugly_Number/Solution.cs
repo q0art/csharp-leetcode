f74a10a339b88eac844a5ace7ce4bfbddea38fd8
Artem Merkulov <qtqt.art@gmail.com>
2023-12-17 15:39:57 +0300

    add 263. Ugly Number sol

---
+++ csharp-leetcode/easy/263.Ugly_Number/Solution.cs
@@ -0,0 +1,29 @@
+namespace csharp_leetcode.easy._263.Ugly_Number;
+
+public class Solution
+{
+    public bool IsUgly(int n) 
+    {
+        if (n == 0)
+        {
+            return false;
+        }
+
+        while (n % 2 == 0)
+        {
+            n /= 2;
+        }
+
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+
+        while (n % 5 == 0)
+        {
+            n /= 5;
+        }
+
+        return n == 1;
+    }
+}
