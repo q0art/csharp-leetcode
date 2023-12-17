eb88f469782cdd40f9b6444749770a5d642cf7f7
Artem Merkulov <qtqt.art@gmail.com>
2023-12-17 15:14:55 +0300

    add 257. Binary Tree Paths sol

---
+++ csharp-leetcode/easy/257.Binary_Tree_Paths/Solution.cs
@@ -0,0 +1,33 @@
+using csharp_leetcode.helpers;
+
+namespace csharp_leetcode.easy._257.Binary_Tree_Paths;
+
+public class Solution
+{
+    public IList<string> BinaryTreePaths(TreeNode root)
+    {
+        var list = new List<string>();
+        
+        BinaryTreePathsHelper(root, "", list);
+
+        return list;
+    }
+
+    private void BinaryTreePathsHelper(TreeNode node, string s, List<string> list)
+    {
+        if (node.left == null && node.right == null)
+        {
+            list.Add(s + node.val);
+        }
+
+        if (node.left != null)
+        {
+            BinaryTreePathsHelper(node.left, s + node.val + "->", list);
+        }
+
+        if (node.right != null)
+        {
+            BinaryTreePathsHelper(node.right, s + node.val + "->", list);
+        }
+    }
+}
