17b084396c3da8463a0097390c90fd9fcddd9d5f
Artem Merkulov <qtqt.art@gmail.com>
2023-12-18 23:39:36 +0300

    add 278. First Bad Version sol

---
+++ csharp-leetcode/easy/268.Missing_Number/Solution.cs
@@ -0,0 +1,9 @@
+namespace csharp_leetcode.easy._268.Missing_Number;
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        return nums.Length * (nums.Length + 1) / 2 - nums.Sum();
+    }
+}
---
+++ csharp-leetcode/easy/278.First_Bad_Version/Solution.cs
@@ -0,0 +1,33 @@
+namespace csharp_leetcode.easy._278.First_Bad_Version;
+
+public class VersionControl
+{
+    public bool IsBadVersion(int n)
+    {
+        return false;
+    }
+}
+
+public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        int left = 0, right = n;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (IsBadVersion(mid))
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
