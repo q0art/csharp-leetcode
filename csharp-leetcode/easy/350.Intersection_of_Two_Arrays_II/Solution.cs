d76834164adea5908f6aaa7c57dd032b631d6bb1
Artem Merkulov <qtqt.art@gmail.com>
2023-12-20 00:21:23 +0300

    add 350. Intersection of Two Arrays II sol

---
+++ csharp-leetcode/easy/350.Intersection_of_Two_Arrays_II/Solution.cs
@@ -0,0 +1,21 @@
+namespace csharp_leetcode.easy._350.Intersection_of_Two_Arrays_II;
+
+public class Solution
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        var result = new List<int>();
+        var list = new List<int>(nums2);
+
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            if (list.Contains(nums1[i]))
+            {
+                result.Add(nums1[i]);
+                list.Remove(nums1[i]);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
