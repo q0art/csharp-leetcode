52563565b6667b3086dbba2e0c2d732acd6bced5
Artem Merkulov <qtqt.art@gmail.com>
2023-12-28 23:14:29 +0300

    add 506. Relative Ranks sol

---
+++ csharp-leetcode/easy/506.Relative_Ranks/Solution.cs
@@ -0,0 +1,29 @@
+using System.Globalization;
+
+namespace csharp_leetcode.easy._506.Relative_Ranks;
+
+public class Solution
+{
+    public string[] FindRelativeRanks(int[] score)
+    {
+        var dict = new Dictionary<int, int>();
+        var array = new string[score.Length];
+
+        for (int i = 0; i < score.Length; i++)
+            dict[score[i]] = i;
+        
+        Array.Sort(score, (a, b) => b - a);
+
+        for (int i = 0; i < score.Length; i++)
+            if (i == 0)
+                array[dict[score[i]]] = "Gold Medal";
+            else if (i == 1)
+                array[dict[score[i]]] = "Silver Medal";
+            else if (i == 2)
+                array[dict[score[i]]] = "Bronze Medal";
+            else
+                array[dict[score[i]]] = (i + 1).ToString();
+
+        return array;
+    }
+}
