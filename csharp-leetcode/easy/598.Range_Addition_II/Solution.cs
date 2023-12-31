e25874dae1721ac1ef17616eea875d1315e0feac
Artem Merkulov <qtqt.art@gmail.com>
2023-12-31 17:37:01 +0300

    add 598. Range Addition II sol

---
+++ csharp-leetcode/easy/598.Range_Addition_II/Solution.cs
@@ -0,0 +1,15 @@
+namespace csharp_leetcode.easy._598.Range_Addition_II;
+
+public class Solution
+{
+    public int MaxCount(int m, int n, int[][] ops) 
+    {
+        foreach (int[] num in ops)
+        {
+            m = Math.Min(m, num[0]);
+            n = Math.Min(n, num[1]);
+        }
+
+        return m * n;
+    }
+}
