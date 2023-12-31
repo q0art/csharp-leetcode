c927bf2284612f0f1d3396392a461c28f6fdd20f
Artem Merkulov <qtqt.art@gmail.com>
2023-12-31 18:19:05 +0300

    add 605. Can Place Flowers sol

---
+++ csharp-leetcode/easy/605.Can_Place_Flowers/Solution.cs
@@ -0,0 +1,18 @@
+namespace csharp_leetcode.easy._605.Can_Place_Flowers;
+
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.Length; i++)
+            if (flowerbed[i] == 0 && 
+                flowerbed.ElementAtOrDefault(i + 1) == 0 &&
+                flowerbed.ElementAtOrDefault(i - 1) == 0)
+            {
+                n--;
+                flowerbed[i] = 1;
+            }
+        
+        return n <= 0;
+    }
+}
