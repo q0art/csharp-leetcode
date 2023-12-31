8c6cb8209fc4c89e6583acc945a182f56fa2e804
Artem Merkulov <qtqt.art@gmail.com>
2023-12-31 19:46:22 +0300

    add 628. Maximum Product of Three Numbers sol

---
+++ csharp-leetcode/easy/628.Maximum_Product_of_Three_Numbers/Solution.cs
@@ -0,0 +1,15 @@
+namespace csharp_leetcode.easy._628.Maximum_Product_of_Three_Numbers;
+
+public class Solution
+{
+    public int MaximumProduct(int[] nums)
+    {
+        var n = nums.Length;
+        
+        Array.Sort(nums);
+
+        return Math.Max(nums[n - 1] * nums[0] * nums[1], 
+            nums[n - 3] * nums[n - 2] * nums[n - 1]);
+        
+    }
+}
