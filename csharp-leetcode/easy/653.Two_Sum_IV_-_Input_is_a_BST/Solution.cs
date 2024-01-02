ba2c0f3ba7d63e8aec6bf499b8947258ef6ab411
Artem Merkulov <qtqt.art@gmail.com>
2024-01-02 23:44:35 +0300

    add 653. Two Sum IV - Input is a BST sol

---
+++ csharp-leetcode/easy/653.Two_Sum_IV_-_Input_is_a_BST/Solution.cs
@@ -0,0 +1,22 @@
+using csharp_leetcode.helpers;
+
+namespace csharp_leetcode.easy._653.Two_Sum_IV___Input_is_a_BST;
+
+public class Solution
+{
+    private HashSet<int> _hashSet = new();
+    
+    public bool FindTarget(TreeNode root, int k)
+    {
+        if (root == null)
+            return false;
+
+        if (_hashSet.Contains(k - root.val))
+            return true;
+
+        _hashSet.Add(root.val);
+
+        return FindTarget(root.left, k) ||
+               FindTarget(root.right, k);
+    }
+}
