cae92974bec803e22ab73115a0e5704b3be7c0ae
Artem Merkulov <qtqt.art@gmail.com>
2024-01-03 21:04:09 +0300

    add 682. Baseball Game sol

---
+++ csharp-leetcode/easy/682.Baseball_Game/Solution.cs
@@ -0,0 +1,21 @@
+namespace csharp_leetcode.easy._682.Baseball_Game;
+
+public class Solution
+{
+    public int CalPoints(string[] operations)
+    {
+        var list = new List<int>();
+
+        foreach (string operation in operations)
+            if (operation == "+")
+                list.Add(list[^1] + list[^2]);
+            else if (operation == "D")
+                list.Add(list[^1] * 2);
+            else if (operation == "C")
+                list.Remove(list[^1]);
+            else
+                list.Add(int.Parse(operation));
+
+        return list.Sum();
+    }
+}
