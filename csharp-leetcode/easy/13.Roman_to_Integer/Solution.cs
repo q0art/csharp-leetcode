f994b3e38a4b98ba1bf7686161a904e209dccbc2
Artem Merkulov <qtqt.art@gmail.com>
2024-01-04 02:00:53 +0300

    13. Roman to Integer to fp

--- csharp-leetcode/easy/13.Roman_to_Integer/Solution.cs
+++ csharp-leetcode/easy/13.Roman_to_Integer/Solution.cs
@@ -2,28 +2,25 @@ namespace csharp_leetcode.easy._13.Roman_to_Integer;
 
 public class Solution 
 {
-    public int RomanToInt(string s)
+    private readonly Dictionary<char, int> dict = new()
     {
-        int result = 0;
-        var dict = new Dictionary<char, int>
-        {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
 
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (i < s.Length - 1 && dict[s[i]] < dict[s[i + 1]])
-                result -= dict[s[i]];
-            else
-                result += dict[s[i]];
-        }
+    public int RomanToInt(string s) {
+        var selected = s.Select((v, i) => new { Value = dict[v], Index = i });
 
-        return result;
+        var result = selected.Select(item =>
+            (s.Length - 1 > item.Index && dict[s[item.Index + 1]] > item.Value)
+                ? item.Value * -1
+                : item.Value);
+
+        return result.Sum();
     }
 }
