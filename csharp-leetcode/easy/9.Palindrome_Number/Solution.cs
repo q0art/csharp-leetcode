cac68d6b6d9f3add64d198ee8c542d4b6f387292
Artem Merkulov <qtqt.art@gmail.com>
2024-01-04 01:28:02 +0300

    9. Palindrome Number to fp

--- csharp-leetcode/easy/9.Palindrome_Number/Solution.cs
+++ csharp-leetcode/easy/9.Palindrome_Number/Solution.cs
@@ -1,15 +1,9 @@
 namespace csharp_leetcode.easy._9.Palindrome_Number;
 
-public class Solution 
-{
-    public bool IsPalindrome(int x)
-    {
-        var s = x.ToString();
-
-        for (int i = 0; i < s.Length / 2; i++)
-            if (s[i] != s[s.Length - i - 1])
-                return false;
-        
-        return true;
-    }
+public class Solution {
+    public bool IsPalindrome(int x) => x
+        .ToString()
+        .Zip(x.ToString().Reverse(), 
+            (c, reversedChar) => c == reversedChar)
+        .All(_ => _);
 }
