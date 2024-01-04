5c2ce5f94cf9c455e28b2c329d0a25580987c53e
Artem Merkulov <qtqt.art@gmail.com>
2024-01-04 21:46:19 +0300

    fix 13. Roman to Integer

--- csharp-leetcode/easy/13.Roman_to_Integer/Solution.cs
+++ csharp-leetcode/easy/13.Roman_to_Integer/Solution.cs
@@ -1,15 +1,15 @@
 namespace csharp_leetcode.easy._13.Roman_to_Integer;
 
 public class Solution {
-    private readonly Dictionary<char, int> dict = new() {
+    private readonly Dictionary<char, int> _dict = new() {
         {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
     };
 
     public int RomanToInt(string s) {
-        var selected = s.Select((v, i) => new { Value = dict[v], Index = i });
+        var selected = s.Select((v, i) => new { Value = _dict[v], Index = i });
 
         var result = selected.Select(item =>
-            (s.Length - 1 > item.Index && dict[s[item.Index + 1]] > item.Value)
+            (s.Length - 1 > item.Index && _dict[s[item.Index + 1]] > item.Value)
                 ? item.Value * -1
                 : item.Value);
 
